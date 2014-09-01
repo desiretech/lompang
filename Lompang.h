0bde4c443ee4e4491b3b34cf89660664de8d69e5
Zhongcai Ng <ngzhongcai@hotmail.com>
2014-09-01 22:15:31 +0800

    APNS
    APNS

--- Lompang.h
+++ Lompang.h
@@ -2,22 +2,19 @@
 //  Lompang.h
 //  Lompang
 //
-//  Created by Zhongcai on 22/6/14.
-//  Copyright (c) 2014 Zhongcai. All rights reserved.
+//  Created by Zhongcai Ng on 1/9/14.
+//  Copyright (c) 2014 lompang. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import <CoreLocation/CoreLocation.h>
 
 @protocol LompangDelegate <NSObject>
 @required
 
--(void)socketConnected;
+-(void)socketConnected:(NSDictionary *)dict;
 -(void)socketDisconnected;
 -(void)socketReceivedErr:(NSDictionary *)dict;
--(void)socketSignedIn:(NSDictionary *)dict;
--(void)socketSignedOut:(NSDictionary *)dict;
--(void)socketRegistered;
+-(void)socketRegistered:(NSDictionary *)dict;
 -(void)socketUnregistered;
 -(void)socketReceivedDict:(NSDictionary *)dict;
 
@@ -27,21 +24,17 @@
 
 @property (nonatomic, weak) id delegate;
 
--(void)connect;
+-(void)connectToAccount:(NSString *)acct WithToken:(NSString *)token;
 -(void)disconnect;
--(void)signInAs:(NSString *)user;
 -(void)update:(NSMutableDictionary *)userload;
--(void)registerApns:(NSData *)token;
+-(void)registerApns:(NSData *)deviceToken;
 -(void)unregisterApns;
 -(void)join:(NSString *)group;
 -(void)unjoin:(NSString *)group;
 -(void)listen:(NSString *)group;
 -(void)unlisten:(NSString *)group;
 -(void)post:(NSString *)pid Pidload:(NSMutableDictionary *)pidload To:(NSString *)group;
--(void)message:(NSString *)message Pid:(NSString *)pid Pidload:(NSMutableDictionary *)pidload To:(NSString *)group;
--(void)remove:(NSString *)pid;
--(NSString *)encode:(CLLocation *)loc WithPrecision:(int)precision;
--(CLLocation *)decode:(NSString *)encoded;
--(NSString *)neighbor:(NSString *)encoded Index:(int)index;
+-(void)unpost:(NSString *)pid;
+-(void)notify:(NSString *)message Group:(NSString *)group;
 
 @end
